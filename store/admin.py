dfd55122eccf3db7c827b64695385d312027f226
Wayne Ng Zu <ngzuwayne@gmail.com>
2025-02-04 11:04:50 +0800

    Connected to mysql server

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from . import models
 
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'path', 'user')
+    list_editable = ('type',)
+
 # Register your models here.
-admin.site.register(models.Document)
+admin.site.register(models.Document, DocumentAdmin)
 
 admin.site.register(models.Log)
 
