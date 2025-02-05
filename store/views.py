e9a8c95464f73840a73c02281ee6893f584ed2a8
Wayne Ng Zu <ngzuwayne@gmail.com>
2025-02-05 10:59:24 +0800

    Added get, post, put requests ...

--- store/views.py
+++ store/views.py
@@ -8,23 +8,41 @@ from .serializers import UserSerializer, DocumentSerializer, LogSerializer
 
 
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def user_list(request):
-    queryset = User.objects.all()
-    serializer = UserSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('ok')
 
-@api_view()
+@api_view(['GET', 'PUT'])
 def user_detail(request, id):
     user = get_object_or_404(User, pk=id)
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-@api_view()
+@api_view(['GET', 'POST'])
 def document_list(request):
-    queryset = Document.objects.all()
-    serializer = DocumentSerializer(queryset, many=True)
-    return Response (serializer.data)
+    if request.method == 'GET':
+        queryset = Document.objects.all()
+        serializer = DocumentSerializer(queryset, many=True)
+        return Response (serializer.data)
+    elif request.method == 'POST':
+        serializer = DocumentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view()
 def log_list(request):
