45c322b15eadfe2d3860dd973861dd542886fbe2
Wayne Ng Zu <ngzuwayne@gmail.com>
2025-02-06 17:47:12 +0800

    downloaded nested routers but have not implemented it yet ...

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('users', views.UserViewSet)
 router.register('documents', views.DocumentViewSet)
 router.register('logs', views.LogViewSet)
